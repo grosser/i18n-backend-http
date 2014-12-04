4cfab7d8be3f07b7563b7137e9bdb4eccd40d0ef
Colin Ewen <colin@draecas.com>
2014-12-04 10:15:41 -0500

    Remove appraisals, fix rspec

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ gemspec
 gem 'rspec', '~> 3.1.0'
 gem 'rake'
 gem 'webmock'
-gem 'appraisal'
 gem 'json'
 
 gem "codeclimate-test-reporter", group: :test, require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ CodeClimate::TestReporter.start
 
 require 'webmock/rspec'
 require_relative '../lib/i18n/backend/jargon'
+Dir["./spec/**/*.rb"].each { |f| require f }
 
 I18n::Backend::Jargon.configure do |config|
   config.host = 'http://www.example.com'
